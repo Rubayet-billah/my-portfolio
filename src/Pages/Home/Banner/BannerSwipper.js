a536250f11497f2afed92a2bb1dd0f2062cd9eb5
Rubayet-billah <rubayetbillah909@gmail.com>
2023-03-30 23:39:50 +0600

    sddd

--- src/Pages/Home/Banner/BannerSwipper.js
+++ src/Pages/Home/Banner/BannerSwipper.js
@@ -4,14 +4,14 @@ import "swiper/swiper-bundle.css";
 import { Autoplay } from "swiper";
 import "./BannerSwipper.css";
 import profileImg from "../../../Images/profile.png";
-import image1 from "../../../Images/img1.jpg";
-import image2 from "../../../Images/img2.jpg";
-import image3 from "../../../Images/img3.jpg";
-import image4 from "../../../Images/img4.jpg";
-import image5 from "../../../Images/img5.jpg";
-import image6 from "../../../Images/img6.jpg";
-import image7 from "../../../Images/img7.png";
-import image8 from "../../../Images/img8.png";
+// import image1 from "../../../Images/img1.jpg";
+// import image2 from "../../../Images/img2.jpg";
+// import image3 from "../../../Images/img3.jpg";
+// import image4 from "../../../Images/img4.jpg";
+// import image5 from "../../../Images/img5.jpg";
+// import image6 from "../../../Images/img6.jpg";
+// import image7 from "../../../Images/img7.png";
+// import image8 from "../../../Images/img8.png";
 
 const BannerSwipper = () => {
   // const bannerImages = [
